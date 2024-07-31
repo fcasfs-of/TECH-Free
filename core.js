c6083e47d0fbcd22829961b548f7b507da51b206
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2024-07-31 11:40:04 -0300

    Create core.js

---
+++ core.js
@@ -0,0 +1,20 @@
+ var var_versionpropg="false";
+
+ var var_versplayerpg=0;
+
+var fplayeri;
+
+
+
+
+var settingsplayer={  color_icons:"#63B8FF", color_texts:"#00EEEE", config:{
+fontstyle:"none",
+textdecoration:"none",
+texttransform:"none",
+textbold:true,
+}, controls:true, };
+
+
+
+
+
