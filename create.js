963cb1da54ac753b33092f6f0903e99d0d7547b4
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2024-07-31 11:49:46 -0300

    Create create.js

---
+++ create.js
@@ -0,0 +1,10 @@
+ var inpss454fod5rwles = document.getElementById('formingi');
+ var inpss45435rwles_oid="";//video/*,audio/*
+ var inpss454ftu35rwles_oid="";
+  //if(listfrr_is["parameters"].mode=="1"){        var_versplayerpg=1;   var inpss454ftu35rwles_oid=" de Vídeo";  var inpss45435rwles_oid="video/*";   }
+  //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
+
+inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivo'+inpss454ftu35rwles_oid+'</div></button> ';
+
+
+isopenduuf(document.getElementById("zplayler"));
