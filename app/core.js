6c6a89c9003616d59165a3b41ab0b9dd20d6697e
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2024-08-01 09:39:43 -0300

    Update core.js

--- app/core.js
+++ app/core.js
@@ -1,6 +1,11 @@
- var var_versionpropg="false";  var var_versplayerpg=0;    var fplayeri;
 
-var settingsplayer={  color_icons:"#63B8FF", color_texts:"#00EEEE", config:{fontstyle:"none",textdecoration:"none",texttransform:"none",textbold:true,}, controls:true, };
+var settingsplayer={  color_icons:"FFFFFF", color_texts:"FFFFFF", config:" ", controls:0,  };   var listfrr_is={};   var fdplayeri;
+
+
+function stringno_valtext(id,g) {   var dfyyggdfgetLastdRofw=id;   if(id=="undefined"){     dfyyggdfgetLastdRofw=g;    }       if(id==undefined){     dfyyggdfgetLastdRofw=g;    }    return dfyyggdfgetLastdRofw;    }
 
   function pl_dispNav(a) {  return document.getElementById(a);  }   function fmdplex_loadex_txv(t){  if(t==""){    return "";  }   if(t=="undefined"){    return "";  }     if(t==undefined){    return "";  }   else {  return t; }  }     function create_mdpl_file(ob){  return { get:function(id){  return ob.files[id]; }, get_name:function(id){ return fmdplex_loadex_txv(create_mdpl_file(ob).get(id).name); }, get_size:function(id){ return fmdplex_loadex_txv(create_mdpl_file(ob).get(id).size); },get_type:function(){  return fmdplex_loadex_txv(create_mdpl_file(ob).get(id).type);  }, create_url:function(id){  return fmdplex_loadex_txv(URL.createObjectURL(create_mdpl_file(ob).get(id))); } };   }
+
+
+
 
