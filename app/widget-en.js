b7416b7efb50c5a3f274039db48df0d9e8e23baa
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-14 08:03:48 -0300

    Update widget-en.js

--- app/widget-en.js
+++ app/widget-en.js
@@ -29,8 +29,13 @@ function openf_osdcd(plsayer, timf,obb,kl){     var openf_osdcd_time=timf;
 
 
 
-function opendtNav() {  var inpmyNavrunzz = document.getElementById("myNavrun");
-inpmyNavrunzz.innerHTML= '<div id="myNav" class="overlay">  <button style="cursor:pointer;" id="zass" class="closebtn csdslosebtn" onclick="closeNav(\'true\');" data-tooltip="Close File" data-flow="left">X</button>  <div class="overlay-content" style="transition: 0.2s;">  <span id="zplaylert"></span>  <div id="zplayler" style="width:100%;  height:100%;  transition: 0.2s;"></div>  </div>  </div>';  
+function opendtNav(fgf) {  var inpmyNavrunzz = document.getElementById("myNavrun");   var dopendtNav="File";
+if(fgf){
+  if(Number(fgf)==1){  dopendtNav="File"; }
+  else if(Number(fgf)>=2){  dopendtNav="Playlist"; }
+}
+                         
+inpmyNavrunzz.innerHTML= '<div id="myNav" class="overlay">  <button style="cursor:pointer;" id="zass" class="closebtn csdslosebtn" onclick="closeNav(\'true\');" data-tooltip="Close '+dopendtNav+'" data-flow="left">X</button>  <div class="overlay-content" style="transition: 0.2s;">  <span id="zplaylert"></span>  <div id="zplayler" style="width:100%;  height:100%;  transition: 0.2s;"></div>  </div>  </div>';  
 var inpsspldsetcusotmdfsdfszz = document.getElementById("pldsetcusotm");    inpsspldsetcusotmdfsdfszz.innerHTML="";
 if(settingsplayer){
 var settingsplayerhgonfig="";    
