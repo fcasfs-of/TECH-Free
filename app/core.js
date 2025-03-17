695a475b6f00e53e70ebb0325666a86973545e8d
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-17 08:18:36 -0300

    Update core.js

--- app/core.js
+++ app/core.js
@@ -1,26 +1,16 @@
+
 function getUrlParameter(sParam) {  var dgetUrlParameterd="";
-
     var sPageURL = decodeURIComponent(location.href);//window.location.search.substring(1));
-
     var sURLVariables = sPageURL.split('?')[1].split('&');
-
     for (var i = 0; i < sURLVariables.length; i++) {
-
         var sParameterName = sURLVariables[i].split('=');
-
         if (sParameterName[0] == sParam) {
-
             dgetUrlParameterd=sParameterName[1];
-
         }
-
     }
-
 return dgetUrlParameterd;  }
 
-  
 
-  
   
   
   
