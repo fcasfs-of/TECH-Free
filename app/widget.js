c618f42a2883065177733878dec936761c609681
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-17 07:55:05 -0300

    Update widget.js

--- app/widget.js
+++ app/widget.js
@@ -62,17 +62,18 @@ if(settingsplayer){  if(settingsplayer.controls==false){   mdpl_handleselectcont
                                                
 if(cc<1){    }
 
-else if(cc==1){     fplayeri = new Playerjs({vars:api_tok, customtext:{ age:call[0].type },id:evt, nocontrols:mdpl_handleselectcontoels, autoplay:0, loop:0, title:call[0].title, file:call[0].file, poster:"", player:bmg,"url":location.href,  });   startOSD(); 
+else if(cc==1){     fplayeri = new Playerjs({vars:api_tok, customtext:{ age:call[0].type },id:evt, nocontrols:mdpl_handleselectcontoels, autoplay:0, loop:0, title:call[0].title, file:call[0].file, poster:"", player:bmg,"url":location.href,  }); 
 is_playerd_allid(settingsplayer, call, fplayeri);  
- is_playerd_allid(getUrlVars(location.href), call, fplayeri);      
+ is_playerd_allid(getUrlVars(location.href), call, fplayeri);        startOSD("oframezplayler"); 
 
 openf_osdcd(document.getElementById("zplayler"),3e3, document.getElementById("zplaylert"), api_tok, cc);
 }
 
 else if(cc>=2){   
-     fplayeri = new Playerjs({vars:api_tok, customtext:{ age:onget_d(cc) },id:evt, nocontrols:mdpl_handleselectcontoels, autoplay:0, loop:0, title:"", file:call, poster:"", player:bmg,"url":location.href,  });    startOSD();
+     fplayeri = new Playerjs({vars:api_tok, customtext:{ age:onget_d(cc) },id:evt, nocontrols:mdpl_handleselectcontoels, autoplay:0, loop:0, title:"", file:call, poster:"", player:bmg,"url":location.href,  });   
 is_playerd_allid(settingsplayer, call, fplayeri);  
- is_playerd_allid(getUrlVars(location.href), call, fplayeri);   
+ is_playerd_allid(getUrlVars(location.href), call, fplayeri);     startOSD("oframezplayler"); 
+  
  openf_osdcd(document.getElementById("zplayler"),3e3, document.getElementById("zplaylert"), api_tok, cc);   
  }
 }
