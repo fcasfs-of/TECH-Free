75124bb0e821a7a2c076a68800d6f2486a6f3328
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-17 08:17:32 -0300

    Atualizar o core.js

--- app/core.js
+++ app/core.js
@@ -1,4 +1,29 @@
+function getUrlParameter(sParam) {  var dgetUrlParameterd="";
 
+    var sPageURL = decodeURIComponent(location.href);//window.location.search.substring(1));
+
+    var sURLVariables = sPageURL.split('?')[1].split('&');
+
+    for (var i = 0; i < sURLVariables.length; i++) {
+
+        var sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] == sParam) {
+
+            dgetUrlParameterd=sParameterName[1];
+
+        }
+
+    }
+
+return dgetUrlParameterd;  }
+
+  
+
+  
+  
+  
+  
  
 function convertSecondsDurationto(duration){
      var seconds = duration; 
