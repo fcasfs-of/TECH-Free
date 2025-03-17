889f313ee5e431ca67533759f6ead8ffdb70e819
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-17 08:46:59 -0300

    Update create.js

--- create.js
+++ create.js
@@ -9,14 +9,15 @@ var getdisble=getUrlParameter("btn_lang");
   //if(listfrr_is["parameters"].mode=="1"){        var_versplayerpg=1;   var inpss454ftu35rwles_oid=" de Vídeo";  var inpss45435rwles_oid="video/*";   }
   //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
 
-inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivo'+inpss454ftu35rwles_oid+'</div></button> ';
 
 var sdsgetdisble ='';
 if(fsPageURL.split('?')){
-     //sdsgetdisble ='<br/><br/><button class="btn" onclick="mchangelag(\'/en?'+fsPageURL.split('?')[1]+'\');" style="transition: 0.2s;width:90%"><div style="padding:4px;">Mudar Idioma para Inglês</div></button>';
+     sdsgetdisble ='<br/><br/><button class="btn" onclick="mchangelag(\\\'/en?'+fsPageURL.split('?')[1]+'\\\');" style="transition: 0.2s;width:90%"><div style="padding:4px;">Mudar Idioma para Inglês</div></button>';
 }
 if(getdisble && getdisble=="no"){   sdsgetdisble='';    }
-inpss454fod5rwles.innerHTML=inpss454fod5rwles.innerHTML+sdsgetdisble;
+
+inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivo'+inpss454ftu35rwles_oid+'</div></button> '+sdsgetdisble;
+
 
 
 
