cab5c4c0b6fbf622881515cb182eff57fa24de7b
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-19 08:59:56 -0300

    Update create.js

--- create.js
+++ create.js
@@ -1,12 +1,12 @@
 
 
- var inpss454fod5rwles = document.getElementById('formingi');   var inpss45435rwles_oid="video/*,audio/*,image/*";   var inpss454ftu35rwles_oid="";   var var_versplayerpg=1; 
+ var inpss454fod5rwles = document.getElementById('formingi');   var inpss45435rwles_oid="application/pdf,video/*,audio/*,image/*";   var inpss454ftu35rwles_oid="";   var var_versplayerpg=1; 
 
   //if(listfrr_is["parameters"].mode=="1"){        var_versplayerpg=1;   var inpss454ftu35rwles_oid=" de Vídeo";  var inpss45435rwles_oid="video/*";   }
   //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
 
 
-inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos (Video, Audio e Image)'+inpss454ftu35rwles_oid+'</div></button> ';
+inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos (Video, Audio, PDF e Image)'+inpss454ftu35rwles_oid+'</div></button> ';
 
 
 //isopenduuf(document.getElementById("zplayler")); 
