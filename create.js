ae6c3a7bb5a3a25d840dd7c03901b23c1cd19575
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-03-20 09:10:13 -0300

    Update create.js

--- create.js
+++ create.js
@@ -5,8 +5,9 @@
   //if(listfrr_is["parameters"].mode=="1"){        var_versplayerpg=1;   var inpss454ftu35rwles_oid=" de Vídeo";  var inpss45435rwles_oid="video/*";   }
   //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
 
+var banner_d= '<img src="player_banner.jpg" style="display: block;    position: fixed;    top: 50%;    left: 0;    bottom: 0;    right: 0;    z-index: -5;    width: 100%;    transform: translateY(-50%);    opacity: 0.7;    url(\"player_banner.jpg\");"/>';
 
-inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos (Video, Audio e Image)'+inpss454ftu35rwles_oid+'</div></button> ';
+inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos (Video, Audio e Image)'+inpss454ftu35rwles_oid+'</div></button> '+banner_d;
 
 
 //isopenduuf(document.getElementById("zplayler")); 
