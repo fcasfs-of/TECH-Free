9de022e6348e1de66570883c6c8b44a999f90273
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-17 20:52:58 -0300

    Atualizar o 99e84d5d.js

--- _file/99e84d5d.js
+++ _file/99e84d5d.js
@@ -1,18 +1,19 @@
 
-document.title= "" + document.title;
+document.title= "Tokusatsu Música em Português" + document.title;
 
 var playerhj = fs_Playerjs({ OSD:true, id:"plo", config:{
  fontweight:"bold",
 
 }, nocontrols:0, autoplay:0, loop:0, title:"", file:[
-
+{title:"Jaspion%20-%20Singles%20Nacionais",file:"https%3A%2F%2Farchive.org%2Fdownload%2Fjaspion-singles-nacionais%2FJaspion%20-%20Singles%20Nacionais.mp4"},
+{title:"Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001",file="https%3A%2F%2Farchive.org%2Fdownload%2Ftokusatsu-singles-nacionais-dvd-01%2FTokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001.mp4"}
 ]
   });
 
 
 playerhj.OnEvents("init",function(){
 
-fplayeri.OnEvents("volume",function(){     fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: 'Volume: '+Math.floor(playerhj.api("volume")*100)+"%", pos: osdposito, showAction: false,  actionText: "", width: 'auto'    });      });
+playerhj.OnEvents("volume",function(){     fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: 'Volume: '+Math.floor(playerhj.api("volume")*100)+"%", pos: osdposito, showAction: false,  actionText: "", width: 'auto'    });      });
   
 playerhj.OnEvents("seek",function(){      fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: ''+convertSecondsDurationto(playerhj.api("time")), pos: osdposito, showAction: false,  actionText: "", width: '180px'   });      });
 
