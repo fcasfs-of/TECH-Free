aa60718c9f0182e7fa9dc4aff36438513bf2f557
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-17 20:45:07 -0300

    Atualizar o 99e84d5d.js

--- _file/99e84d5d.js
+++ _file/99e84d5d.js
@@ -1,3 +1,26 @@
 
+document.title= "" + document.title;
+
+var playerhj = fs_Playerjs({ OSD:true, id:"plo", config:{
+ fontweight:"bold",
+
+}, nocontrols:0, autoplay:0, loop:0, title:"", file:[
+
+]
+  });
 
 
+playerhj.OnEvents("init",function(){
+
+fplayeri.OnEvents("volume",function(){     fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: 'Volume: '+Math.floor(playerhj.api("volume")*100)+"%", pos: osdposito, showAction: false,  actionText: "", width: 'auto'    });      });
+  
+playerhj.OnEvents("seek",function(){      fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: ''+convertSecondsDurationto(playerhj.api("time")), pos: osdposito, showAction: false,  actionText: "", width: '180px'   });      });
+
+playerhj.OnEvents("userseek",function(){      fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: ''+convertSecondsDurationto(playerhj.api("time")), pos: osdposito, showAction: false,  actionText: "", width: '180px'   });      });
+
+playerhj.OnEvents("speed",function(){       fs_OSD({ duration:openf_osdcd_time, text: 'Speed: '+playerhj.api("speed"), position:osdpositdo, pos: osdposito, showAction: false,  actionText: "", width: 'auto'     });       });
+  
+});
+  
+  
+      
