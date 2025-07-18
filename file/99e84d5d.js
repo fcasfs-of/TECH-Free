68f02071a4249ad9946aae6bf8d969dce1b39d53
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-18 14:15:19 -0300

    Atualizar o 99e84d5d.js

--- file/99e84d5d.js
+++ file/99e84d5d.js
@@ -12,12 +12,12 @@ player_lang:"",
 {
 poster:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_000177.jpg",
 title:"Jaspion - Singles Nacionais",
-file:"https://archive.org/download/jaspion-singles-nacionais/Jaspion - Singles Nacionais.mp4"
+file:"https://archive.org/download/jaspion-singles-nacionais/Jaspion%20-%20Singles%20Nacionais.mp4"
 },
 {
 poster:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/tokusatsu-singles-nacionais-dvd-01.thumbs/Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001_003719.jpg",
 title:"Tokusatsu - Singles Nacionais - DVD 01",
-file:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/Tokusatsu - Singles Nacionais - DVD 01.mp4"
+file:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001.mp4"
 }
 ],
  cover:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_001135.jpg"
