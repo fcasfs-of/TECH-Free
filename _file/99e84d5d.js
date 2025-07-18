6a0d34b26509fb71fca8f24c661f7105c4fb2c74
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-18 07:59:07 -0300

    Atualizar o 99e84d5d.js

--- _file/99e84d5d.js
+++ _file/99e84d5d.js
@@ -6,9 +6,9 @@ var playerhj = fs_Playerjs({ OSD:true, id:"zplayler", config:{
   coloricons:"FFD700",
   colortexts:"fff"
 }, nocontrols:0, autoplay:0, loop:0, title:"Tokusatsu Música em Português", file:[
-{title:"Jaspion - Singles Nacionais",file:"https://archive.org/download/jaspion-singles-nacionais/Jaspion - Singles Nacionais.mp4"},
-{title:"Tokusatsu - Singles Nacionais - DVD 01",file:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/Tokusatsu - Singles Nacionais - DVD 01.mp4"}
-], poster:"","embed":location.href, "url":location.href
+{poster:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_000177.jpg",title:"Jaspion - Singles Nacionais",file:"https://archive.org/download/jaspion-singles-nacionais/Jaspion - Singles Nacionais.mp4"},
+{poster:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/tokusatsu-singles-nacionais-dvd-01.thumbs/Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001_003719.jpg",title:"Tokusatsu - Singles Nacionais - DVD 01",file:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/Tokusatsu - Singles Nacionais - DVD 01.mp4"}
+], poster:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_001135.jpg","embed":location.href, "url":location.href
   });
 
 
