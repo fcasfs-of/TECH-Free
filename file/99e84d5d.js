01e29712a3e9a1375fa76908541c95046a549980
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-18 08:24:17 -0300

    Update 99e84d5d.js

--- file/99e84d5d.js
+++ file/99e84d5d.js
@@ -1,14 +1,24 @@
 
-document.title= "Tokusatsu Música em Português - " + document.title;
-
-var playerhj = fs_Playerjs({ OSD:true, id:"zplayler", config:{
+var filedru={
+ file_title:"Tokusatsu Música em Português",
+ config:{
  fontweight:"bold",
   coloricons:"FFD700",
   colortexts:"fff"
-}, nocontrols:0, autoplay:0, loop:0, title:"Tokusatsu Música em Português", file:[
+},
+ list:[
 {poster:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_000177.jpg",title:"Jaspion - Singles Nacionais",file:"https://archive.org/download/jaspion-singles-nacionais/Jaspion - Singles Nacionais.mp4"},
 {poster:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/tokusatsu-singles-nacionais-dvd-01.thumbs/Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001_003719.jpg",title:"Tokusatsu - Singles Nacionais - DVD 01",file:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/Tokusatsu - Singles Nacionais - DVD 01.mp4"}
-], poster:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_001135.jpg","embed":location.href, "url":location.href
+],
+ cover:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_001135.jpg"
+};
+
+
+
+function runEx(){
+document.title= filedru.file_title+" - " + document.title;
+
+var playerhj = fs_Playerjs({ OSD:true, id:"zplayler", config:filedru.config, nocontrols:0, autoplay:0, loop:0, title:filedru.file_title, file:filedru.list, poster:filedru.cover,"embed":location.href, "url":location.href
   });
 
 
@@ -28,6 +38,8 @@ playerhj.OnEvents("speed",function(){       fs_OSD({ duration:openf_osdcd_time, 
 });
 
    openf_osdcd(document.getElementById("zplayler"),3e3, "", [], 0);   
+
+}
 
   
       
