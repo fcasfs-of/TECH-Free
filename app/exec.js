0a362075a63380662ae18e3153d388b15f7525cf
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-18 06:51:34 -0300

    Atualizar o exec.js

--- app/exec.js
+++ app/exec.js
@@ -11,13 +11,16 @@ var scriptfd = document.createElement("script");
         scriptfd.setAttribute("type", "text/javascript");
         scriptfd.setAttribute("src", "https://player.fcasfs-of.cloud-fs.net/_file/"+getval_tyget+".js");
 
-        inpmyNavruz.appendChild(scriptfd);
+        document.head.appendChild(scriptfd);
 
 
 var scriptd = document.createElement("div");
 scriptd.style.display="none"; scriptd.innerHTML='<style>#plo, #zplayler, #plo #zplayler{position:absolute;width:100%; top:0px;left:0px;bottom:0px;right:0px; height:100%;z-index:1;}</style>';
 
         inpmyNavruz.appendChild(scriptd);
+
+
+if(runEx){  runEx();  }
 
 
 }
