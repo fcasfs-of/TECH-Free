af69d2f9da9da9e541c46df521f5e49e989c2121
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-18 09:10:56 -0300

    Update 99e84d5d.js

--- file/99e84d5d.js
+++ file/99e84d5d.js
@@ -1,5 +1,5 @@
 
-var filedru={
+function run_file(){  return { 
  file_title:"Tokusatsu Música em Português",
  config:{
  fontweight:"bold",
@@ -11,35 +11,8 @@ var filedru={
 {poster:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/tokusatsu-singles-nacionais-dvd-01.thumbs/Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001_003719.jpg",title:"Tokusatsu - Singles Nacionais - DVD 01",file:"https://archive.org/download/tokusatsu-singles-nacionais-dvd-01/Tokusatsu - Singles Nacionais - DVD 01.mp4"}
 ],
  cover:"https://archive.org/download/jaspion-singles-nacionais/jaspion-singles-nacionais.thumbs/Jaspion%20-%20Singles%20Nacionais_001135.jpg"
-};
+};  }
 
 
 
-function runEx(){
-document.title= filedru.file_title+" - " + document.title;
-
-var playerhj = fs_Playerjs({ OSD:true, id:"zplayler", config:filedru.config, nocontrols:0, autoplay:0, loop:0, title:filedru.file_title, file:filedru.list, poster:filedru.cover,"embed":location.href, "url":location.href
-  });
-
-
-var fplayeri = playerhj;
-
-
-playerhj.OnEvents("init",function(){
-
-playerhj.OnEvents("volume",function(){     fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: 'Volume: '+Math.floor(playerhj.api("volume")*100)+"%", pos: osdposito, showAction: false,  actionText: "", width: 'auto'    });      });
-  
-playerhj.OnEvents("seek",function(){      fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: ''+convertSecondsDurationto(playerhj.api("time")), pos: osdposito, showAction: false,  actionText: "", width: '180px'   });      });
-
-playerhj.OnEvents("userseek",function(){      fs_OSD({ duration:openf_osdcd_time, position:osdpositdo, text: ''+convertSecondsDurationto(playerhj.api("time")), pos: osdposito, showAction: false,  actionText: "", width: '180px'   });      });
-
-playerhj.OnEvents("speed",function(){       fs_OSD({ duration:openf_osdcd_time, text: 'Speed: '+playerhj.api("speed"), position:osdpositdo, pos: osdposito, showAction: false,  actionText: "", width: 'auto'     });       });
-  
-});
-
-   openf_osdcd(document.getElementById("zplayler"),3e3, "", [], 0);   
-
-}
-
-  
       
