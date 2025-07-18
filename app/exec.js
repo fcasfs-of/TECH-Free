c2545e82fdc3de8d8950430907a5b9ff594c4da0
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-18 08:08:45 -0300

    Update exec.js

--- app/exec.js
+++ app/exec.js
@@ -3,7 +3,7 @@ var getval_tyget=getUrlParameter("fileID");
 if (getval_tyget!="") {
 
 
-var inpmyNavruz = document.getElementById("plo");    
+var inpmyNavruz = document.getElementById("myNavrun");    
 inpmyNavruz.innerHTML= '<div id="zplayler" style="width:100%; height:100%;  transition: 0.2s;"></div>';  
 
 
@@ -17,7 +17,7 @@ document.getElementsByTagName("body")[0].appendChild(scriptfd);
 
 var scriptd = document.createElement("div");
   scriptd.style.display="none";
-  scriptd.innerHTML='<style>#plo, #zplayler, #plo #zplayler{position:absolute;width:100%; top:0px;left:0px;bottom:0px;right:0px; height:100%;z-index:1;}</style>';
+  scriptd.innerHTML='<style>#myNavrun{ display:block; }   #myNavrun, #zplayler, #myNavrun #zplayler{position:absolute;width:100%; top:0px;left:0px;bottom:0px;right:0px; height:100%;z-index:1;}</style>';
 document.getElementsByTagName("body")[0].appendChild(scriptd);
 
 
