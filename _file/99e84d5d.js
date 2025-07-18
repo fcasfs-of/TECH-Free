054780f66bc063af5b0ae35c8a16cb07080d9bc6
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-17 21:24:39 -0300

    Atualizar o 99e84d5d.js

--- _file/99e84d5d.js
+++ _file/99e84d5d.js
@@ -1,9 +1,8 @@
 
 document.title= "Tokusatsu Música em Português - " + document.title;
 
-var playerhj = fs_Playerjs({ OSD:true, id:"myNavrun", config:{
- fontweight:"bold",
-
+var playerhj = fs_Playerjs({ OSD:true, id:"zplayler", config:{
+ fontweight:"bold"
 }, nocontrols:0, autoplay:0, loop:0, title:"", file:[
 {title:"Jaspion%20-%20Singles%20Nacionais",file:"https%3A%2F%2Farchive.org%2Fdownload%2Fjaspion-singles-nacionais%2FJaspion%20-%20Singles%20Nacionais.mp4"},
 {title:"Tokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001",file:"https%3A%2F%2Farchive.org%2Fdownload%2Ftokusatsu-singles-nacionais-dvd-01%2FTokusatsu%20-%20Singles%20Nacionais%20-%20DVD%2001.mp4"}
