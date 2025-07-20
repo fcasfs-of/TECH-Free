3905262dffe8bfced0248cbc82b44cd81af55db3
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-20 20:23:35 -0300

    Criar 99e84d6k.js

---
+++ file/99e84d6k.js
@@ -0,0 +1,28 @@
+
+function run_file(){  return { 
+player_lang:"en",
+pg_dark:true,
+player_osd:true,
+player_controls:0,
+player_autoplay:0,
+player_loop:0,
+ file_title:"Saikyou Saikou Super Stars - theme MV",
+ file_desc:"Super Sentai Strongest Battle",
+ config:{
+ fontweight:"bold",
+  coloricons:"fff",
+  colortexts:"fff"
+},
+ list:[
+{
+poster:"https://archive.org/download/vcd-04-tokusatsu_202504/vcd-04-tokusatsu_202504.thumbs/Super%20Sentai%20Strongest%20Battle%20-%20Saikyou%20Saikou%20Super%20Stars%20_%20theme%20MV_000057.jpg",
+title:"Super Sentai Strongest Battle",
+file:"https://archive.org/download/vcd-04-tokusatsu_202504/Super%20Sentai%20Strongest%20Battle%20-%20Saikyou%20Saikou%20Super%20Stars%20_%20theme%20MV.ia.mp4"
+}
+],
+ cover:"https://archive.org/download/vcd-04-tokusatsu_202504/vcd-04-tokusatsu_202504.thumbs/Super%20Sentai%20Strongest%20Battle%20-%20Saikyou%20Saikou%20Super%20Stars%20_%20theme%20MV_000057.jpg"
+};  }
+
+
+
+      
