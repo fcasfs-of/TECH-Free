e7900faa180cef3e790d0beb7cdac81d261b2c60
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-21 20:34:05 -0300

    Criar 99e84d6ja.js

---
+++ file/99e84d6ja.js
@@ -0,0 +1,33 @@
+
+function run_file(){  return { 
+player_lang:"en",
+pg_dark:true,
+player_osd:true,
+player_controls:0,
+player_autoplay:0,
+player_loop:0,
+ file_title:"Juspion - 2x1",
+ file_desc:"Vídeo Clipe s",
+ config:{
+ fontweight:"bold",
+  coloricons:"00FFFF",
+  colortexts:"FFFF00"
+},
+ list:[
+{
+poster:"https://archive.org/download/vcd-04-tokusatsu_202504/vcd-04-tokusatsu_202504.thumbs/Juspion%20VS%20Mad%20Gallant_000087.jpg",
+title:"Juspion VS Mad Galant",
+file:"https://archive.org/download/vcd-04-tokusatsu_202504/Juspion%20VS%20Mad%20Gallant.mp4"
+},
+{
+poster:"https://archive.org/download/vcd-04-tokusatsu_202504/vcd-04-tokusatsu_202504.thumbs/Daileon%20VS%20Satan%20Gorth_000417.jpg",
+title:"Daileon VS Satan Gorth",
+file:"https://archive.org/download/vcd-04-tokusatsu_202504/Daileon%20VS%20Satan%20Gorth.mp4"
+}
+],
+ cover:"https://archive.org/download/vcd-04-tokusatsu_202504/vcd-04-tokusatsu_202504.thumbs/Juspion%20VS%20Mad%20Gallant_000267.jpg"
+};  }
+
+
+
+      
