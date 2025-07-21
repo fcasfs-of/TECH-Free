7823ee7161c2a3c9da87c416d2bf345725c65cc7
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-21 14:20:57 -0300

    Atualizar o exec.js

--- app/exec.js
+++ app/exec.js
@@ -110,6 +110,8 @@ document.getElementsByTagName("body")[0].appendChild(scriptfd);
 
 var exec_tun="";
 if (getval_tygepret=="true") {      exec_tun=" runEx(run_file, degetval_tselett, degetval_tseposttt);  ";    }
+if (getval_tygepret=="on") {      exec_tun=" runEx(run_file, degetval_tselett, degetval_tseposttt);  ";    }
+
   
 var scrfiptfd = document.createElement("script");
     scrfiptfd.setAttribute("type", "text/javascript");
