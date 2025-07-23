d21ca5dcbb059d93a09c11f66f25df27cdab1299
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-23 11:34:42 -0300

    Update create.js

--- create.js
+++ create.js
@@ -6,10 +6,10 @@
   //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
 
 var valir_odf="";   var valiposr_odf="1";
-var getval_tidyget=getUrlParameter("id");
-var getval_dftidyget=getUrlParameter("pos");
-if(getval_tidyget){   if (getval_tidyget!="") {   valir_odf=getval_tidyget;  }    }
-if(getval_dftidyget){   if (getval_dftidyget!="") {   valiposr_odf=getval_dftidyget;  }    }
+//var getval_tidyget=getUrlParameter("id");
+//var getval_dftidyget=getUrlParameter("pos");
+//if(getval_tidyget){   if (getval_tidyget!="") {   valir_odf=getval_tidyget;  }    }
+//if(getval_dftidyget){   if (getval_dftidyget!="") {   valiposr_odf=getval_dftidyget;  }    }
 
 inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos <br/> (Video, Audio e Image)'+inpss454ftu35rwles_oid+'</div></button><br/><hr/><form class="busca" action="https://player.fcasfs-of.cloud-fs.net/?fileView=true&"><input type="text" id="textidf" placeholder="ID..." name="fileID" value="'+valir_odf+'"/><label class="container" data-tooltip="Visualização" data-flow="top"><input type="checkbox" checked="true" valeu="true" name="fileView"/><span class="checkmark"></span></label><label class="range" data-tooltip="Posição" data-flow="bottom"><input type="range" min="1" max="99" value="'+valiposr_odf+'" name="fileSelect" id="fileSelect"/><span class="value" style="color:#000;"></span></label><button type="submit" value="Buscar">Buscar</button></form>  <br/>  ';
 
