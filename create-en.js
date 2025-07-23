d23a0eec5a9deafdadf4e89c85f92915f54408af
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-23 11:11:41 -0300

    Update create-en.js

--- create-en.js
+++ create-en.js
@@ -6,7 +6,15 @@
   //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
 
 
-inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Open Files <br/> (Video, Audio and Image)'+inpss454ftu35rwles_oid+'</div></button><br/><hr/><form class="busca" action="https://player.fcasfs-of.cloud-fs.net/en?fileView=true&"><input type="text" placeholder="ID..." name="fileID"/><label class="container" data-tooltip="Preview" data-flow="top"><input type="checkbox" checked="true" valeu="true" name="fileView"/><span class="checkmark"></span></label><label class="range" data-tooltip="Position" data-flow="bottom"><input type="range" min="1" max="99" value="1" name="fileSelect" id="fileSelect"/><span class="value" style="color:#000;"></span></label><button type="submit" value="Search">Search</button></form>  <br/> ';
+inpss454fod5rwles.innerHTML='<inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Open Files <br/> (Video, Audio and Image)'+inpss454ftu35rwles_oid+'</div></button><br/><hr/><form class="busca" action="https://player.fcasfs-of.cloud-fs.net/en?fileView=true&"><input type="text" placeholder="ID..." id="textidf" name="fileID"/><label class="container" data-tooltip="Preview" data-flow="top"><input type="checkbox" checked="true" valeu="true" name="fileView"/><span class="checkmark"></span></label><label class="range" data-tooltip="Position" data-flow="bottom"><input type="range" min="1" max="99" value="1" name="fileSelect" id="fileSelect"/><span class="value" style="color:#000;"></span></label><button type="submit" value="Search">Search</button></form>  <br/> ';
+
+ var inpsobdles = document.getElementById('textidf');
+ var inpsobdldfes = document.getElementById('fileSelect');
+var getval_tidyget=getUrlParameter("id");
+if (getval_tidyget!="") {   inpsobdles.value=getval_tidyget;  }
+var getval_dftidyget=getUrlParameter("pos");
+if (getval_dftidyget!="") {   inpsobdldfes.value=getval_dftidyget;  }
+
 
 progressScriptfile("#fileSelect",".value");
 
