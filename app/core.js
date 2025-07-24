d8d1dbfa4ec1567ab5537d39ee2556033d0a985e
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-24 08:44:33 -0300

    Update core.js

--- app/core.js
+++ app/core.js
@@ -2,9 +2,17 @@
 var app_ver="2.0.0.1";
 
 
+function progress_valo(mumh) {  var mumhf=0;    
+if(mumh){   mumhf=mumh;
+if(mumh<=9){  mumhf="0"+mumh;  }
+}
+return mumhf;  }
+
+
+
 function progressScript(idf,ll) {
     const progress = (idf.value / idf.max) * 100;
-  ll.textContent = idf.value;
+  ll.textContent = progress_valo(idf.value);
   idf.style.background = `linear-gradient(to right, #4682B4 ${progress}%, #ccc ${progress}%)`;
  }
 
@@ -13,7 +21,7 @@ function progressScriptfile(oper,valks){
 const sliderValue = document.querySelector(valks);
 sliderEl.addEventListener("input", (event) => {
   const tempSliderValue = event.target.value; 
-  sliderValue.textContent = tempSliderValue;
+  sliderValue.textContent = progress_valo(tempSliderValue);
   const progress = (tempSliderValue / sliderEl.max) * 100;
   sliderEl.style.background = `linear-gradient(to right, #4682B4 ${progress}%, #ccc ${progress}%)`;
 });
