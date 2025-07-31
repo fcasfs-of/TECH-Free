865a84dd0503bd8314336b09d66f43ba77b74b57
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-31 10:46:53 -0300

    Update create-app.js

--- create-app.js
+++ create-app.js
@@ -1,6 +1,5 @@
 
-
- var inpss454fod5rwles = document.getElementById('formingi');   var inpss45435rwles_oid="video/*,audio/*,image/*";   var inpss454ftu35rwles_oid="";   var var_versplayerpg=1; 
+ var inpss454fod5rwles = document.getElementById('formingi');   var inpss45435rwles_oid="video/*,audio/*,image/*,application/pdf";   var inpss454ftu35rwles_oid="";   var var_versplayerpg=1; 
 
   //if(listfrr_is["parameters"].mode=="1"){        var_versplayerpg=1;   var inpss454ftu35rwles_oid=" de Vídeo";  var inpss45435rwles_oid="video/*";   }
   //if(listfrr_is["parameters"].mode=="2"){     var_versplayerpg=2;   var inpss454ftu35rwles_oid=" de Áudio";  var inpss45435rwles_oid="audio/*";   }
@@ -11,9 +10,7 @@ var valir_odf="";   var valiposr_odf="1";
 //if(getval_tidyget){   if (getval_tidyget!="") {   valir_odf=getval_tidyget;  }    }
 //if(getval_dftidyget){   if (getval_dftidyget!="") {   valiposr_odf=getval_dftidyget;  }    }
 
-inpss454fod5rwles.innerHTML='<div class="busca" style="display:none;margin: 0 auto;width: 100%;"><label class="container" data-tooltip="OSD" data-flow="bottom" for="osd_display"><input type="checkbox" checked="true" id="osd_display"><span class="checkmark"></span></label></div><br/>  <inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos <br/> (Video, Audio e Image)'+inpss454ftu35rwles_oid+'</div></button><br/>  <br/>  ';
-
-
+inpss454fod5rwles.innerHTML='<div class="busca" style="display:none;margin: 0 auto;width: 100%;"><label class="container" data-tooltip="OSD" data-flow="bottom" for="osd_display"><input type="checkbox" checked="true" id="osd_display"><span class="checkmark"></span></label></div><br/>  <inp'+'ut type="file" class="lk" id="inpssle" multiple="multiple" name="files" accept="'+inpss45435rwles_oid+'"/><br/><button class="btn" onclick="run_modelop();" id="infdfpssle" style="transition: 0.2s;width:90%"><div style="padding:4px;">Abrir Arquivos <br/> (PDF, Video, Audio e Image)'+inpss454ftu35rwles_oid+'</div></button><br/>  <br/>  ';
 
 //isopenduuf(document.getElementById("zplayler")); 
 
