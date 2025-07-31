63f152f0c39bc31c208c870d5024e7c0b387309e
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-07-31 11:33:13 -0300

    Update widget.js

--- app/widget.js
+++ app/widget.js
@@ -25,6 +25,140 @@ call(mdpl_handlefFileSelectsa, evt.files.length, mkk, jj);
 
 
 
+
+
+
+function laoded_app_pdf(PDFFILE){
+
+function dataURItoBinArray(data) {
+  var binary = atob(data);
+  var array = [];
+  for (var i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i));
+  }
+  return new Uint8Array(array);
+}
+
+document.querySelector("#opendoc").addEventListener("change", function (e) {
+  let file = e.target;
+  let reader = new FileReader();
+  reader.onload = async function () {
+    await pdfViewer.loadDocument({
+      data: dataURItoBinArray(reader.result.replace(/^data:.*;base64,/, ""))
+    });
+    await pdfThumbnails
+      .loadDocument({
+        data: dataURItoBinArray(reader.result.replace(/^data:.*;base64,/, ""))
+      })
+      .then(() => pdfThumbnails.setZoom("fit"));
+  };
+  if (file.files.length > 0) {
+    reader.readAsDataURL(file.files[0]);
+    document.querySelector("#filedownload").download = document.querySelector(
+      "#opendoc"
+    ).files[0].name;
+  }
+});
+
+
+function setHorizontal() {
+  document.querySelector(".maindoc").classList.add("horizontal-scroll");
+  pdfViewer.refreshAll();
+}
+/** Toggles the visibility of the thumbnails */
+function togglethumbs(el) {
+  if (el.classList.contains("pushed")) {
+    el.classList.remove("pushed");
+    document.querySelector(".thumbnails").classList.add("hide");
+  } else {
+    el.classList.add("pushed");
+    document.querySelector(".thumbnails").classList.remove("hide");
+  }
+}
+function hideselected() {
+  let $selected = pdfThumbnails.$container.find(".selected");
+  let i = $selected.data("page");
+  $selected.toggleClass("hidden");
+  pdfViewer.$container
+    .find('.pdfpage[data-page="' + i + '"]')
+    .toggleClass("hidden");
+  pdfViewer.scrollToPage(i);
+}
+let pdfViewer = new PDFjsViewer($(".maindoc"), {
+  zoomValues: [0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4],
+
+  onZoomChange: function (zoom) {
+    zoom = parseInt(zoom * 10000) / 100;
+    $(".zoomval").text(zoom + "%");
+  },
+
+  onActivePageChanged: function (page) {
+    let pageno = $(page).data("page");
+    let pagetotal = this.getPageCount();
+
+    if (!$(page).hasClass("hidden")) {
+      pdfThumbnails.setActivePage(pageno);
+      $("#pageno").val(pageno);
+      $("#pageno").attr("max", pagetotal);
+      $("#pagecount").text("de " + pagetotal);
+    }
+  },
+
+  onDocumentReady: function () {
+    pdfViewer.setZoom("fit");
+    pdfViewer.pdf.getData().then(function (data) {
+      document.querySelector("#filedownload").href = URL.createObjectURL(
+        new Blob([data], { type: "application/pdf" })
+      );
+      document.querySelector("#filedownload").target = "_blank";
+    });
+  }
+});
+
+pdfViewer.loadDocument(PDFFILE).then(function () {
+  //document.querySelector("#filedownload").download = PDFFILE;
+});
+
+let pdfThumbnails = new PDFjsViewer($(".thumbnails"), {
+  zoomFillArea: 0.7,
+  onNewPage: function (page, i) {
+    $('<div class="numbering">').text(i).appendTo(page);
+    page.on("click", function () {
+      pdfThumbnails.setActivePage(page.data("page"));
+      if (!pdfViewer.isPageVisible(page.data("page"))) {
+        pdfViewer.scrollToPage(page.data("page"));
+      }
+    });
+  },
+  onDocumentReady: function () {
+    this.setZoom("fit");
+  }
+});
+
+pdfThumbnails.setActivePage = function (pageno) {
+  this.$container.find(".pdfpage").removeClass("selected");
+
+  let $npage = this.$container
+    .find('.pdfpage[data-page="' + pageno + '"]')
+    .addClass("selected");
+  $("#hideselected").removeClass("pushed");
+  if ($npage.hasClass("hidden")) $("#hideselected").addClass("pushed");
+
+  if (!this.isPageVisible(pageno)) {
+    this.scrollToPage(pageno);
+  }
+}.bind(pdfThumbnails);
+
+pdfThumbnails.loadDocument(PDFFILE);
+
+}
+
+
+
+
+
+
+
  function openf_osdcd(plobf, plsayer, timf,obb,kl){     var openf_osdcd_time=timf;
 
   if(obb){  obb.innerHTML="";   }
@@ -328,140 +462,6 @@ isopenduuf(insss667ddd);
     fileInput.files = e.dataTransfer.files;
     fileInput.onchange(fileInput);
   });
-
-
-
-
-
-
-
-function laoded_app_pdf(PDFFILE){
-
-function dataURItoBinArray(data) {
-  var binary = atob(data);
-  var array = [];
-  for (var i = 0; i < binary.length; i++) {
-    array.push(binary.charCodeAt(i));
-  }
-  return new Uint8Array(array);
-}
-
-document.querySelector("#opendoc").addEventListener("change", function (e) {
-  let file = e.target;
-  let reader = new FileReader();
-  reader.onload = async function () {
-    await pdfViewer.loadDocument({
-      data: dataURItoBinArray(reader.result.replace(/^data:.*;base64,/, ""))
-    });
-    await pdfThumbnails
-      .loadDocument({
-        data: dataURItoBinArray(reader.result.replace(/^data:.*;base64,/, ""))
-      })
-      .then(() => pdfThumbnails.setZoom("fit"));
-  };
-  if (file.files.length > 0) {
-    reader.readAsDataURL(file.files[0]);
-    document.querySelector("#filedownload").download = document.querySelector(
-      "#opendoc"
-    ).files[0].name;
-  }
-});
-
-
-function setHorizontal() {
-  document.querySelector(".maindoc").classList.add("horizontal-scroll");
-  pdfViewer.refreshAll();
-}
-/** Toggles the visibility of the thumbnails */
-function togglethumbs(el) {
-  if (el.classList.contains("pushed")) {
-    el.classList.remove("pushed");
-    document.querySelector(".thumbnails").classList.add("hide");
-  } else {
-    el.classList.add("pushed");
-    document.querySelector(".thumbnails").classList.remove("hide");
-  }
-}
-function hideselected() {
-  let $selected = pdfThumbnails.$container.find(".selected");
-  let i = $selected.data("page");
-  $selected.toggleClass("hidden");
-  pdfViewer.$container
-    .find('.pdfpage[data-page="' + i + '"]')
-    .toggleClass("hidden");
-  pdfViewer.scrollToPage(i);
-}
-let pdfViewer = new PDFjsViewer($(".maindoc"), {
-  zoomValues: [0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4],
-
-  onZoomChange: function (zoom) {
-    zoom = parseInt(zoom * 10000) / 100;
-    $(".zoomval").text(zoom + "%");
-  },
-
-  onActivePageChanged: function (page) {
-    let pageno = $(page).data("page");
-    let pagetotal = this.getPageCount();
-
-    if (!$(page).hasClass("hidden")) {
-      pdfThumbnails.setActivePage(pageno);
-      $("#pageno").val(pageno);
-      $("#pageno").attr("max", pagetotal);
-      $("#pagecount").text("de " + pagetotal);
-    }
-  },
-
-  onDocumentReady: function () {
-    pdfViewer.setZoom("fit");
-    pdfViewer.pdf.getData().then(function (data) {
-      document.querySelector("#filedownload").href = URL.createObjectURL(
-        new Blob([data], { type: "application/pdf" })
-      );
-      document.querySelector("#filedownload").target = "_blank";
-    });
-  }
-});
-
-pdfViewer.loadDocument(PDFFILE).then(function () {
-  //document.querySelector("#filedownload").download = PDFFILE;
-});
-
-let pdfThumbnails = new PDFjsViewer($(".thumbnails"), {
-  zoomFillArea: 0.7,
-  onNewPage: function (page, i) {
-    $('<div class="numbering">').text(i).appendTo(page);
-    page.on("click", function () {
-      pdfThumbnails.setActivePage(page.data("page"));
-      if (!pdfViewer.isPageVisible(page.data("page"))) {
-        pdfViewer.scrollToPage(page.data("page"));
-      }
-    });
-  },
-  onDocumentReady: function () {
-    this.setZoom("fit");
-  }
-});
-
-pdfThumbnails.setActivePage = function (pageno) {
-  this.$container.find(".pdfpage").removeClass("selected");
-
-  let $npage = this.$container
-    .find('.pdfpage[data-page="' + pageno + '"]')
-    .addClass("selected");
-  $("#hideselected").removeClass("pushed");
-  if ($npage.hasClass("hidden")) $("#hideselected").addClass("pushed");
-
-  if (!this.isPageVisible(pageno)) {
-    this.scrollToPage(pageno);
-  }
-}.bind(pdfThumbnails);
-
-pdfThumbnails.loadDocument(PDFFILE);
-
-}
-
-
-
 
 
 
