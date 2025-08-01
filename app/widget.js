cf4c528f02b52303416fbe19e2504f9244242397
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-08-01 11:46:42 -0300

    Update widget.js

--- app/widget.js
+++ app/widget.js
@@ -278,11 +278,11 @@ else if(cc==1){       var img_ond="";
 
 if(call[0].type && call[0].type==="application/x-zip-compressed"){
    document.getElementById(evt).innerHTML='<div id="result"> <br/> <div id="file-name"></div> <br/> <div>    <ul id="file-list" style="list-style-type: none;  padding: 0;  margin: 0;">    </ul>  </div>  <div id="inner-file-name"></div>  <br/>  <div id="file-content"></div>  <br/></div>';
-  var fileListUl = document.getElementById('file-list');    clearResult(fileListUl);
+  const fileListUl = document.getElementById('file-list');    clearResult(fileListUl);
   fs_loadZip(ongh[0], false);
 }   else if(call[0].type && call[0].type==="application/zip"){
    document.getElementById(evt).innerHTML='<div id="result"> <br/> <div id="file-name"></div> <br/> <div>    <ul id="file-list" style="list-style-type: none;  padding: 0;  margin: 0;">    </ul>  </div>  <div id="inner-file-name"></div>  <br/>  <div id="file-content"></div>  <br/></div>';
-  var fileListUl = document.getElementById('file-list');    clearResult(fileListUl);
+  const fileListUl = document.getElementById('file-list');    clearResult(fileListUl);
   fs_loadZip(ongh[0], false);
 }            
 else if(call[0].type && call[0].type==="application/pdf"){
