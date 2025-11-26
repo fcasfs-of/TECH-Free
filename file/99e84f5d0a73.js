2ab287979719baaad673851ffedce948b683cf3b
FCAS_FS <fcasfs.of@cloud-fs.net>
2025-11-26 20:21:53 -0300

    Atualizar o 99e84f5d0a73.js

--- file/99e84f5d0a73.js
+++ file/99e84f5d0a73.js
@@ -1,1 +1,50 @@
 
+function run_file(){  return { 
+player_lang:"en", 
+pg_dark:true,
+player_osd:true,
+player_controls:0,
+player_autoplay:0,
+player_loop:0,
+ file_title:"Tokusatsu: Metal Heroes - Hit Song Collection",
+ file_desc:"<div><b>ALL </b><u>TOKUSATSU</u>: <u><b>METAL HEROES</b></u></div><div><u><b><br /></b></u></div><div><u><b>____________________________________________________</b></u></div><div><u><br /></u></div><ul><li>B-Fighter Kabuto</li><li>Blue SWAT</li><li>Choujinki Metalder</li><li>Heavy Shell B-Fighter</li><li>Jikuu Senshi Spielvan</li><li>Kidou Keiji Jiban</li><li>Kyojuu Tokusou Juspion</li><li>Sekai Ninja Sen Jiraiya</li><li>Special Investigation Robo Janperson</li><li>Special Rescue Exceedraft</li><li>Tokkei Winspector</li><li>Tokkyuu Shirei Solbrain</li><li>Uchuu Keiji Gavan</li><li>Uchuu Keiji Shaider</li><li>Uchuu Keiji Sharivan</li></ul>",
+config:{
+ osd:{
+  theme:"light"
+ },
+ fontweight:"bold"
+},
+ list:[
+{
+poster:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vcd-02-tokusatsu-metalheroes_202405.thumbs/vid-002-metalheroes%20%282%29_0_001976.jpg",
+title:"Part 01",
+file:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vid-002-metalheroes%20%282%29_0.ia.mp4"
+},
+{
+poster:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vcd-02-tokusatsu-metalheroes_202405.thumbs/vid-002-metalheroes%20%282%29_1_002094.jpg",
+title:"Part 02",
+file:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vid-002-metalheroes%20%282%29_1.ia.mp4"
+},
+{
+poster:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vcd-02-tokusatsu-metalheroes_202405.thumbs/vid-002-metalheroes%20%282%29_2_001858.jpg",
+title:"Part 03",
+file:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vid-002-metalheroes%20%282%29_2.ia.mp4"
+},
+{
+poster:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vcd-02-tokusatsu-metalheroes_202405.thumbs/vid-002-metalheroes%20%282%29_3_000418.jpg",
+title:"Part 04",
+file:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vid-002-metalheroes%20%282%29_3.ia.mp4"
+},
+{
+poster:”https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vcd-02-tokusatsu-metalheroes_202405.thumbs/vid-002-metalheroes%20%282%29_4_000654.jpg",
+title:"Part 05",
+file:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vid-002-metalheroes%20%282%29_4.ia.mp4"
+}
+],
+ cover:"https://archive.org/download/vcd-02-tokusatsu-metalheroes_202405/vcd-02-tokusatsu-metalheroes_202405.thumbs/vid-002-metalheroes%20%282%29_0_000714.jpg"
+};  }
+
+
+
+
+
