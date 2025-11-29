19a6fdece4bff9879b50957eb76b912f5bfb77f5
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2025-11-29 11:42:51 -0300

    Atualizar o 99f84mh00.js

--- file/99f84mh00.js
+++ file/99f84mh00.js
@@ -1,2 +1,30 @@
 
+function run_file(){  return { 
+player_lang:"en", 
+pg_dark:true,
+player_osd:true,
+player_controls:0,
+player_autoplay:0,
+player_loop:0,
+ file_title:"Uchuu Keiji Sharivan - OST",
+ file_desc:"Collection Soundtrack - OST",
+config:{
+ osd:{
+  theme:"light"
+ },
+ fontweight:"bold"
+},
+ list:[
+{
+poster:"https://archive.org/download/tokusatsu-collection-soundtrack-ost_1of/Uchuu%20Keiji%20Sharivan_spectrogram.png",
+title:"Uchuu Keiji Sharivan",
+file:"https://archive.org/download/tokusatsu-collection-soundtrack-ost_1of/Uchuu%20Keiji%20Sharivan.mp3"
+}
+
+],
+ cover:"https://archive.org/download/tokusatsu-collection-soundtrack-ost_1of/Uchuu%20Keiji%20Sharivan_spectrogram.png"
+};  }
+
+
+
 
