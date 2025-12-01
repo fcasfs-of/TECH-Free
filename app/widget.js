44da757118a164a3ec449d65ba1465b9420a56da
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2025-12-01 10:31:30 -0300

    Update widget.js

--- app/widget.js
+++ app/widget.js
@@ -327,7 +327,9 @@ is_playerd_allid(settingsplayer, call, fplayeri);
             }
             
             function loadURL() {
-                const url = urlInput.value.trim();
+var inpmyNavrunzz = document.getElementById("myNavrun");   var dopendtNav="Arquivo";
+              
+              const url = urlInput.value.trim();
                 
                 if (!url) {
                     showError("Por favor, insira uma URL.");
@@ -342,11 +344,12 @@ is_playerd_allid(settingsplayer, call, fplayeri);
                 errorMessage.style.display = 'none';
                 loadBtn.disabled = true;
                 loadBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Carregando...';
+              
      document.body.style.overflow="hidden";                    
 inpmyNavrunzz.innerHTML= '<div id="myNav" class="overlay">  <button style="cursor:pointer;" id="zass" class="closebtn csdslosebtn" onclick="closeNav(\'true\');" data-tooltip="Fechar '+dopendtNav+'" data-flow="left">X</button>  <div class="overlay-content" style="transition: 0.2s;">  <span id="zplaylert"></span>  <div id="zplayler" style="width:100%;  height:100%;  transition: 0.2s;"></div>  </div>  </div>';  
 
-    fplayeri = fs_Playerjs({ config:settingsplayer, OSD:osddisplay.checked, customtext:{ age:"" }, id:"zplayler", nocontrols:0, autoplay:0, loop:0, casturl:url, castname:"", title:"", file:url, poster:"", player:1,"url":location.href });
-is_playerd_allid(settingsplayer, call, fplayeri);  
+    fplayeri = fs_Playerjs({ config:{}, OSD:true, customtext:{ age:"" }, id:"zplayler", nocontrols:0, autoplay:0, loop:0, casturl:url, castname:"", title:"", file:url, poster:"", player:1,"url":location.href });
+is_playerd_allid(settingsplayer, [], fplayeri);  
                
 openf_osdcd(fplayeri, document.getElementById("zplayler"), 3e3, document.getElementById("zplaylert"), {}, 1);
               
