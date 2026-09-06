837414ad349ad1a000890bc2e0331972c534ecc1
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-06 18:08:46 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -123,7 +123,7 @@ document.querySelector('head').appendChild(ddselement);
 
 
 
-var fs_OSD = function(){};
+var fs_OSD = function(){};   var fs_OSD_hide = function(){};
 
   function startOSD(df, config){
 function get_odsd(id){  var get_odsdhy=document.body;       var insssd = document.getElementById("oframe"+id);     if(insssd){   get_odsdhy=insssd;     }      return get_odsdhy;  }
@@ -148,7 +148,7 @@ a.current.style.top="-100px",a.current.style.bottom="-100px")}.bind(a.snackbar),
                                                                                                                                                                                         
 var c=function(){var a={},b=!1,d=0,e=arguments.length;"[object Boolean]"===Object.prototype.toString.call(arguments[0])&&(b=arguments[0],d++);for(var f=function(d){for(var e in d)Object.prototype.hasOwnProperty.call(d,e)&&(b&&"[object Object]"===Object.prototype.toString.call(d[e])?a[e]=c(!0,a[e],d[e]):a[e]=d[e])};d<e;d++){var g=arguments[d];f(g)}return a};return a});
 
-  fs_OSD=Snackbar.show;   }
+  fs_OSD=Snackbar.show;    fs_OSD_hide=Snackbar.close;  }
 
 
 
